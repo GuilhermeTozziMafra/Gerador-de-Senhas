68a245edc4255988a74a95fda0e24aeb29459c1e
Guilherme Tozzi Mafra <80405986+GuilhermeTozziMafra@users.noreply.github.com>
2022-05-08 18:48:57 -0400

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,22 @@
+ let input = document.querySelector('#password'),
+        info = document.querySelector('#info'),
+        button = document.querySelector('button')
+    pass_size = 8;
+
+    input.value = passwordGenerator();
+
+    function passwordGenerator() {
+        let randomPass;
+
+        do {
+            randomPass = Math.random().toString(32).substr(2)
+        } while (randomPass.length > pass_size);
+
+        return randomPass;
+    }
+
+    button.addEventListener('click', function() {
+        input.value = passwordGenerator();
+        info.innerText = 'A tua senha tem ' + passwordGenerator().length + ' caracteres'
+    })
 
